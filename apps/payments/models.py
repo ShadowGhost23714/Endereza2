27ee81853829c7a0fd4feea87893a66eda1ca13e
TobosoBest <mreued23@gmail.com>
2026-05-13 15:54:20 -0300

    feat: modelsRegistroPago

--- apps/payments/models.py
+++ apps/payments/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class PagoEfectivo (models.Model): 
+    #Relaciono el pago con un turno unico
+    turno= models.OneToOneField(Turno, on_delete=models.CASCADE, related_name='pago')
+
+    #Monto pagado
+    monto= models.DecimalField(max_digits=10, decimal_places=2)
+
+    #Fecha y hora
+    fecha_pago= models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"Pago de ${self.monto} para el turno {self.turno.id} realizado el {self.fecha_pago}"
