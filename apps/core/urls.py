deb790b8c21538e2132627a32c4aa91bd09d370f
Aarón J. M. D'Amico <Shadowghost23714@gmail.com>
2026-05-23 14:54:48 -0300

    Revert "Merge: Se agregan las funcionalidades relacionadas con las clases a dev"

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -3,7 +3,4 @@ from . import views
 
 app_name = "core"
 
-urlpatterns = [
-    path("", views.home, name="home"),
-]
-
+urlpatterns = [path("", views.index, name="index")]
--- apps/turnos_view/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TurnosViewConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.turnos_view" 
