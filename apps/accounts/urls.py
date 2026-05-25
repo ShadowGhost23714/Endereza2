75484f94574caa8813de5a539e663580cdf7d876
ShadowGhost23714 <shadowghost23714@gmail.com>
2026-05-25 14:01:49 -0300

    fix: saque codigo viejo y url entendible

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 from .views import CustomLoginView, CustomLogoutView, CustomPasswordChangeView
-from django.contrib.auth import logout
 
 app_name = "accounts"
 
@@ -10,8 +9,8 @@ urlpatterns = [
     path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
-    path('secretario/', views.secretario, name='secretario'),
-    path('secretario/lista/', views.lista_secretarios, name='lista_secretarios'),  # ← AGREGAR
-    path('secretario/<int:pk>/eliminar/', views.eliminar_secretario, name='eliminar_secretario'),  # ← AGREGAR
+    path('secretario/nuevo/', views.secretario, name='secretario'),
+    path('secretario/lista/', views.lista_secretarios, name='lista_secretarios'),
+    path('secretario/<int:pk>/eliminar/', views.eliminar_secretario, name='eliminar_secretario'),
     path('change_password/', CustomPasswordChangeView.as_view(), name="change_password")
 ]
