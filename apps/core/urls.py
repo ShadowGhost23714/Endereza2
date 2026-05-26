5fffa6428f0a81455c677d5e439904d6704b455c
GregorioVeraza <168499718+GregorioVeraza@users.noreply.github.com>
2026-05-26 16:52:15 -0300

    refactor: Simplify home view routing based on user role

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = "core"
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("dueño/", views.dueño, name="dueño")
+    path("", views.HomeRouterView.as_view(), name="home"),
 ]
 
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth import get_user_model
+from django.views import View
+
 User = get_user_model()
 
 User = get_user_model()
@@ -18,3 +20,10 @@ def turnos(request):
 def dueño(request):
     usuarios = User.objects.all()
     return render(request, 'core/dueño.html', {'usuarios': usuarios})
+
+class HomeRouterView(View):
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user.is_superuser:
+            return dueño(request)
+        else:
+            return home(request)
