4b982dfaf6bd7699a31a093c214b496bfbef4a79
ShadowGhost23714 <shadowghost23714@gmail.com>
2026-05-26 01:02:31 -0300

    feat: credenciales de mercado pago en el config

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,11 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+# MercadoPago Credentials
+MP_PUBLIC_KEY = config("MP_PUBLIC_KEY")
+MP_ACCESS_TOKEN = config("MP_ACCESS_TOKEN")
+
+
 ALLOWED_HOSTS = []
 
 
