cf2617632eccbb40688f427bbe2ffa49e442d378
ShadowGhost23714 <shadowghost23714@gmail.com>
2026-06-18 21:37:21 -0300

    feat: funcion para crear preferencia

--- apps/payments/services.py
+++ apps/payments/services.py
@@ -1,7 +1,9 @@
 from django.db import transaction
 from django.shortcuts import get_object_or_404
-from classes.models import Reserva
+from apps.classes.models import Reserva
 from .models import Pago
+import mercadopago
+from django.conf import settings
 
 def procesar_pago_presencial(reserva_id, monto, metodo_pago="efectivo"):
     with transaction.atomic():
@@ -24,3 +26,32 @@ def procesar_pago_presencial(reserva_id, monto, metodo_pago="efectivo"):
         reserva.save()
 
         return nuevo_pago
+    
+
+# Función para crear la preferencia de pago en MercadoPago
+def crear_preferencia(reserva, request):
+    sdk = mercadopago.SDK(settings.MP_ACCESS_TOKEN)
+
+    base_url = "https://extinct-stem-sporting.ngrok-free.dev"
+
+    preference_data = {
+        "items": [{
+            "id": str(reserva.id),
+            "title": f"Reserva #{reserva.id} — {reserva.id_turno.get_actividad_display()}",
+            "quantity": 1,
+            "unit_price": float(reserva.id_turno.precio),
+            "currency_id": "ARS",
+        }],
+        "back_urls": {
+            "success": f"{base_url}/pagos/exito/",
+            "failure": f"{base_url}/pagos/error/",
+            "pending": f"{base_url}/pagos/pendiente/",
+        },
+        "auto_return": "approved",
+        "external_reference": str(reserva.id),
+        "notification_url": request.build_absolute_uri("/pagos/webhook/"),
+    }
+
+    response = sdk.preference().create(preference_data)
+    print("RESPUESTA COMPLETA DE MP:", response)  # ← agregar esto
+    return response["response"]
